4323ba9db14f0b0ff4fe86dce89db8e5cb2cbbbc
mikhaeris <mikhail.kiselev.dev@gmail.com>
2025-08-09 09:29:27 +0300

    fix: yazi and tmux

--- .config/yazi/wrapper.sh
+++ .config/yazi/wrapper.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 function _ya() {
-  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-  yazi "$@" --cwd-file="$tmp"
-  IFS= read -r -d '' cwd < "$tmp"
-  [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+  local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+  $@ --cwd-file="$tmp"
+  if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    cd "$cwd"
+  fi
   rm -f -- "$tmp"
 }
 
-function ya()  { _ya yazi "$@" }
+function ya() { _ya yazi "$@" }
 function sya() { _ya sudo -E yazi "$@" }
-
