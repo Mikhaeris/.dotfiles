c742d63826c65fc47d31b35704d45149c7c6c075
mikhaeris <mikhail.kiselev.dev@gmail.com>
2026-02-09 20:10:27 +0300

    fix(nvim): vimtex style code

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -4,37 +4,25 @@ return {
 	init = function()
 		vim.g.vimtex_view_method = "zathura"
 		vim.g.vimtex_compiler_method = "latexmk"
+		vim.g.vimtex_quickfix_mode = 0
 
 		vim.g.vimtex_compiler_clean_on_terminate = 1
-    vim.g.vimtex_quickfix_mode = 0
+
+		vim.g.vimtex_compiler_latexmk_engines = {
+			_ = "-xelatex",
+		}
 
 		vim.g.vimtex_compiler_latexmk = {
-      out_dir = 'build',
+			out_dir = "build",
+			executable = "latexmk",
 			options = {
+				"-pdfxe",
 				"-shell-escape",
 				"-verbose",
 				"-file-line-error",
 				"-synctex=1",
 				"-interaction=nonstopmode",
-				"-pdfxe",
 			},
-		}
-
-		-- vim.g.vimtex_compiler_latexmk = {
-		-- build_dir = "build",
-		-- executable = "latexmk",
-		-- options = {
-		-- "-synctex=1",
-		-- "-interaction=nonstopmode",
-		-- "-file-line-error",
-		-- "-pvc",
-		--},
-
-		vim.g.vimtex_compiler_latexmk_engines = {
-			_ = "-xelatex",
-			pdflatex = "-pdf",
-			lualatex = "-lualatex",
-			xelatex = "-xelatex",
 		}
 	end,
 }
