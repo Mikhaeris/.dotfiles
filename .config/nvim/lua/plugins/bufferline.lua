eb8c04482f3b414bef01ddc4359e2d18263e3c37
mikhaeris <mikhail.kiselev.dev@gmail.com>
2026-02-22 22:39:46 +0300

    feat(nvim): bufferline lazy false

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -2,7 +2,7 @@ return {
   "akinsho/bufferline.nvim",
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
-  lazy = true,
+  lazy = false,
   opts = {
     options = {
       mode = "buffers",
