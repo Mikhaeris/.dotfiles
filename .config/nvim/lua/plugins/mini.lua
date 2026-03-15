3747161c2352e92eb2a9f6cf55ff250d4f0d1bf2
mikhaeris <mikhail.kiselev.dev@gmail.com>
2026-03-15 20:29:34 +0300

    feat(nvim): add mini.align

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,11 +1,17 @@
 return {
-  {
-    "echasnovski/mini.bufremove",
-    config = function()
-      require("mini.bufremove").setup()
-      vim.keymap.set("n", "<leader>ct", function()
-        require("mini.bufremove").delete(0, false)
-      end)
-    end,
-  },
+	{
+		"echasnovski/mini.bufremove",
+		config = function()
+			require("mini.bufremove").setup()
+			vim.keymap.set("n", "<leader>ct", function()
+				require("mini.bufremove").delete(0, false)
+			end)
+		end,
+	},
+	{
+		"echasnovski/mini.align",
+		config = function()
+			require("mini.align").setup()
+		end,
+	},
 }
