8e3fba9d5ea77107c0b20ab63057cbbc19f3c01e
mikhaeris <mikhail.kiselev.dev@gmail.com>
2026-04-15 23:44:28 +0300

    feat(scripts): move scripts to local

--- scripts/install/pre_install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-# make directory
-mkdir ~/.local/bin
-mkdir ~/.cache/zsh
-mkdir ~/.cache/bash
--- scripts/throne_post_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo chown root:root /usr/lib/throne/Core
-sudo chmod u+s /usr/lib/throne/Core
-
-sudo chown root:root /opt/Throne/ThroneCore
-sudo chmod u+s /opt/Throne/ThroneCore
