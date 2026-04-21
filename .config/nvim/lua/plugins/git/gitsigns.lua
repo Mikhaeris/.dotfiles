c7b2c22496798ef4a3b7453593c5b865427200bc
mikhaeris <mikhail.kiselev.dev@gmail.com>
2026-04-21 13:04:17 +0300

    feat(nvim): add gitsigns

---
+++ .config/nvim/lua/plugins/git/gitsigns.lua
@@ -0,0 +1,14 @@
+return {
+	"lewis6991/gitsigns.nvim",
+	event = "BufReadPre",
+	opts = {
+		signs = { add = { text = "▎" }, change = { text = "▎" }, delete = { text = "" } },
+		on_attach = function(buf)
+			local gs = package.loaded.gitsigns
+			vim.keymap.set("n", "]h", gs.next_hunk, { buffer = buf, desc = "Next hunk" })
+			vim.keymap.set("n", "[h", gs.prev_hunk, { buffer = buf, desc = "Prev hunk" })
+			vim.keymap.set("n", "<leader>gp", gs.preview_hunk, { buffer = buf, desc = "Preview hunk"})
+			vim.keymap.set("n", "<leader>gb", gs.blame_line, { buffer = buf, desc = "Blame line" })
+		end,
+	},
+}
