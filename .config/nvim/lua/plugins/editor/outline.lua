891e18d81a8a081093d6266183507f15b6d49a51
mikhaeris <mikhail.kiselev.dev@gmail.com>
2026-04-24 13:33:56 +0300

    feat(nvim): add outline

---
+++ .config/nvim/lua/plugins/editor/outline.lua
@@ -0,0 +1,53 @@
+return {
+  "hedyhli/outline.nvim",
+  cmd = "Outline",
+  keys = {
+    { "<leader>os", "<cmd>Outline<cr>", desc = "Toggle Outline" },
+  },
+  opts = {
+    outline_window = {
+      position = "right",
+      width = 30,
+      relative_width = false,
+    },
+    symbol_folding = {
+      autofold_depth = 1,
+    },
+    keymaps = {
+      up_and_jump = "<up>",
+      down_and_jump = "<down>",
+    },
+    symbols = {
+      icons = {
+        File = { icon = "󰈙", hl = "Identifier" },
+        Module = { icon = "󰆧", hl = "Include" },
+        Namespace = { icon = "󰅪", hl = "Include" },
+        Package = { icon = "󰏗", hl = "Include" },
+        Class = { icon = "󰠱", hl = "Type" },
+        Method = { icon = "󰆧", hl = "Function" },
+        Property = { icon = "", hl = "Identifier" },
+        Field = { icon = "󰜢", hl = "Identifier" },
+        Constructor = { icon = "", hl = "Special" },
+        Enum = { icon = "", hl = "Type" },
+        Interface = { icon = "", hl = "Type" },
+        Function = { icon = "󰊕", hl = "Function" },
+        Variable = { icon = "󰀫", hl = "Constant" },
+        Constant = { icon = "󰏿", hl = "Constant" },
+        String = { icon = "󰀬", hl = "String" },
+        Number = { icon = "󰎠", hl = "Number" },
+        Boolean = { icon = "", hl = "Boolean" },
+        Array = { icon = "󰅪", hl = "Constant" },
+        Object = { icon = "󰅩", hl = "Type" },
+        Key = { icon = "󰌋", hl = "Type" },
+        Null = { icon = "󰟢", hl = "Type" },
+        EnumMember = { icon = "", hl = "Identifier" },
+        Struct = { icon = "󰙅", hl = "Type" },
+        Event = { icon = "", hl = "Type" },
+        Operator = { icon = "󰆕", hl = "Identifier" },
+        TypeParameter = { icon = "󰊄", hl = "Identifier" },
+        Component = { icon = "󰡀", hl = "Function" },
+        Fragment = { icon = "󰅴", hl = "Constant" },
+      },
+    },
+  },
+}
